d7a4fe2c89ba067a5ca9f7252e326990a35a7e32
Cameron Keith <cameron.s.keith.26@dartmouth.edu>
2024-04-22 18:02:26 -0400

    added useParams import from react-router-dom

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink, useParams } from 'react-router-dom';
 import './style.scss';
 
 const About = (props) => {
